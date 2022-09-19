ebf3e71b29ac88ac616f9747fc9a3f8fbdb8dc19
Uliana Kolodii <ulianakolodii@gmail.com>
2022-09-19 17:12:08 +0300

    add mode logic

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-// const generateRandomChoice = () => {
-// 	const randomNumber = Math.floor(Math.random() * 2);
-// 	return ["X", "O"][randomNumber];
-//   };
-
 const FBBoxesEl = document.querySelectorAll(".FB_field-box");
 const MBBoxesEl = document.querySelectorAll(".MB_field-box");
 const PCBoxesEl = document.querySelectorAll(".PC_field-box");
 let winnerBoxEl = document.getElementById("winner_box");
-const modeFieldEL = document.getElementById("mode_container")
-const PCButtonEl = document.getElementById("PC_button")
+const modeFieldEL = document.getElementById("mode_container");
+const PCButtonEl = document.getElementById("PC_button");
 const PCFieldEl = document.getElementById("PC_field-container");
-const FBButtonEl = document.getElementById("FB_button")
+const FBButtonEl = document.getElementById("FB_button");
 const FBFieldEl = document.getElementById("FB_field-container");
-const MBButtonEl = document.getElementById("MB_button")
+const MBButtonEl = document.getElementById("MB_button");
 const MBFieldEl = document.getElementById("MB_field-container");
+const modeButtonEl = document.getElementById("mode_button");
+modeButtonEl.style.display = "none";
+FBFieldEl.style.display = "none";
+PCFieldEl.style.display = "none";
+MBFieldEl.style.display = "none";
+Array.from(FBBoxesEl).forEach((el) => (el.innerText = ""));
+Array.from(MBBoxesEl).forEach((el) => (el.innerText = ""));
+Array.from(PCBoxesEl).forEach((el) => (el.innerText = ""));
 
 function showFBMode() {
   FBFieldEl.style.display = "flex";
   PCFieldEl.style.display = "none";
   MBFieldEl.style.display = "none";
   modeFieldEL.style.display = "none";
-} 
+  modeButtonEl.style.display = "flex";
+}
 
-FBButtonEl.addEventListener('click', () => {
+FBButtonEl.addEventListener("click", () => {
   showFBMode();
 });
 
@@ -31,9 +35,10 @@ function showMBMode() {
   PCFieldEl.style.display = "none";
   MBFieldEl.style.display = "flex";
   modeFieldEL.style.display = "none";
-} 
+  modeButtonEl.style.display = "flex";
+}
 
-MBButtonEl.addEventListener('click', () => {
+MBButtonEl.addEventListener("click", () => {
   showMBMode();
 });
 
@@ -42,9 +47,10 @@ function showPCMode() {
   PCFieldEl.style.display = "flex";
   MBFieldEl.style.display = "none";
   modeFieldEL.style.display = "none";
-} 
+  modeButtonEl.style.display = "flex";
+}
 
-PCButtonEl.addEventListener('click', () => {
+PCButtonEl.addEventListener("click", () => {
   showPCMode();
 });
 
@@ -171,74 +177,96 @@ function magicCheck(boxes) {
 }
 
 let i = 0;
-let initial = 'X';
+let initial = "X";
 function FBBoxClick(event) {
   event.target.innerText = initial;
-  initial = initial === 'X' ? 'O': 'X';
+  initial = initial === "X" ? "O" : "X";
   const result = magicCheck(FBBoxesEl);
   if (result !== false) {
     winnerBoxEl.innerHTML = `The winner is ${result}.`;
-    removeEventListeners(FBBoxesEl);
+    FBRemoveEventListeners(FBBoxesEl);
   }
 }
 
-function removeEventListeners(FBBoxesEl) {
-  Array.from(FBBoxesEl).forEach(
-    box => box.removeEventListener('click', FBBoxClick, { once: true })
-  )
+function FBRemoveEventListeners(FBBoxesEl) {
+  Array.from(FBBoxesEl).forEach((box) =>
+    box.removeEventListener("click", FBBoxClick, { once: true })
+  );
 }
 
-function addEventListeners(FBBoxesEl) {
-  Array.from(FBBoxesEl).forEach(
-    box => box.addEventListener('click', FBBoxClick, { once: true })
-  )
+function FBAddEventListeners(FBBoxesEl) {
+  console.log("hello", Array.from(FBBoxesEl));
+  Array.from(FBBoxesEl).forEach((box) =>
+    box.addEventListener("click", FBBoxClick, { once: true })
+  );
 }
-addEventListeners(FBBoxesEl);
+FBAddEventListeners(FBBoxesEl);
+
+const generateRandomChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 2);
+  return ["X", "O"][randomNumber];
+};
 
 function MBBoxClick(event) {
-  event.target.innerText = initial;
-  initial = initial === 'X' ? 'O': 'X';
+  event.target.innerText = generateRandomChoice();
   const result = magicCheck(MBBoxesEl);
   if (result !== false) {
     winnerBoxEl.innerHTML = `The winner is ${result}.`;
-    removeEventListeners(MBBoxesEl);
+    MBRemoveEventListeners(MBBoxesEl);
   }
 }
 
-function removeEventListeners(MBBoxesEl) {
-  Array.from(MBBoxesEl).forEach(
-    box => box.removeEventListener('click', MBBoxClick, { once: true })
-  )
+function MBRemoveEventListeners(MBBoxesEl) {
+  Array.from(MBBoxesEl).forEach((box) =>
+    box.removeEventListener("click", MBBoxClick, { once: true })
+  );
 }
 
-function addEventListeners(MBBoxesEl) {
-  Array.from(MBBoxesEl).forEach(
-    box => box.addEventListener('click', MBBoxClick, { once: true })
-  )
+function MBAddEventListeners(MBBoxesEl) {
+  Array.from(MBBoxesEl).forEach((box) =>
+    box.addEventListener("click", MBBoxClick, { once: true })
+  );
 }
-addEventListeners(MBBoxesEl);
+MBAddEventListeners(MBBoxesEl);
 
 function PCBoxClick(event) {
   event.target.innerText = initial;
-  initial = initial === 'X' ? 'O': 'X';
+  initial = initial === "X" ? "O" : "X";
   const result = magicCheck(PCBoxesEl);
   if (result !== false) {
     winnerBoxEl.innerHTML = `The winner is ${result}.`;
-    removeEventListeners(PCBoxesEl);
+    PCRemoveEventListeners(PCBoxesEl);
   }
 }
 
-function removeEventListeners(PCBoxesEl) {
-  Array.from(PCBoxesEl).forEach(
-    box => box.removeEventListener('click', PCBoxClick, { once: true })
-  )
+function PCRemoveEventListeners(PCBoxesEl) {
+  Array.from(PCBoxesEl).forEach((box) =>
+    box.removeEventListener("click", PCBoxClick, { once: true })
+  );
 }
 
-function addEventListeners(PCBoxesEl) {
-  Array.from(PCBoxesEl).forEach(
-    box => box.addEventListener('click', PCBoxClick, { once: true })
-  )
+function PCAddEventListeners(PCBoxesEl) {
+  Array.from(PCBoxesEl).forEach((box) =>
+    box.addEventListener("click", PCBoxClick, { once: true })
+  );
 }
-addEventListeners(PCBoxesEl);
+PCAddEventListeners(PCBoxesEl);
 
+function showModeField() {
+  FBFieldEl.style.display = "none";
+  PCFieldEl.style.display = "none";
+  MBFieldEl.style.display = "none";
+  modeFieldEL.style.display = "flex";
+  modeButtonEl.style.display = "none";
+  winnerBoxEl.innerText = "";
+  Array.from(FBBoxesEl).forEach((el) => (el.innerText = ""));
+  Array.from(MBBoxesEl).forEach((el) => (el.innerText = ""));
+  Array.from(PCBoxesEl).forEach((el) => (el.innerText = ""));
+  PCAddEventListeners(PCBoxesEl);
+  FBAddEventListeners(FBBoxesEl);
+  MBAddEventListeners(MBBoxesEl);
+}
 
+modeButtonEl.addEventListener("click", () => {
+  showModeField();
+});
